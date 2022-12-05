f6ff13a763263c9d10dd97586c71db7286735063
sieunnnn <wldsmtldsm65@gmail.com>
2022-12-05 19:44:18 +0900

    [Class example] Overloading program

---
+++ src/multiCampus/ex/EX1205_Override_Overloading/A_override/Figure.java
@@ -0,0 +1,12 @@
+package multiCampus.ex.EX1205_Override_Overloading.A_override;
+
+public class Figure {
+
+    public double calArea() {
+        return 0.0;
+    }
+
+    private void testExtends() {
+        System.out.println("private 메서드는 상속이 안됩니다.");
+    }
+}
