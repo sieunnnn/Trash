c0a908d9f66dcda4fd3e325fd449bebd05d9abfc
sieunnnn <wldsmtldsm65@gmail.com>
2022-12-06 12:51:07 +0900

    [Class example] Modify contents

--- src/multiCampus/ex/EX120101.java
+++ src/multiCampus/ex/EX120101.java
@@ -10,7 +10,7 @@ public class EX120101 {
             배열에 저장된 각 데이터들은 해당 데이터에 접근할 수 있는 index 번호를 부여 받는다.
             배열은 참조자료형으로 JVM heap 영역에 저장된다.
             참조형 변수(reference)를 통해 heap 영역의 배열에 접근할 수 있다.
-            index를 통해 배열안의 데이터에 접근할 수 있다.
+            index 를 통해 배열안의 데이터에 접근할 수 있다.
 
          */
 
@@ -30,7 +30,7 @@ public class EX120101 {
         System.out.println(arr[3]);
 
         /*
-         <배열의 선언과 동시에 초기화>
+        <배열의 선언과 동시에 초기화>
             크기가 5인 int[] 을 선언하고 각 인덱스에 5,4,3,2,1 값을 초기화
          */
         int[] iarr = new int[] {5,4,3,2,1};
@@ -83,10 +83,10 @@ public class EX120101 {
 
         copyArr[2] = "장어덮밥";
 
-        System.out.println("foodArr의 주소 : " + System.identityHashCode(foodArr));
+        System.out.println("foodArr 의 주소 : " + System.identityHashCode(foodArr));
         System.out.println(Arrays.toString(copyArr));
 
-        System.out.println("copyArr의 주소 : " + System.identityHashCode(copyArr));
+        System.out.println("copyArr 의 주소 : " + System.identityHashCode(copyArr));
         System.out.println(Arrays.toString(copyArr));
 
 
