4344d09a5bf4fb64ae64014c73b302cf3b1abf64
sieunnnn <wldsmtldsm65@gmail.com>
2022-12-09 16:31:58 +0900

    [1000] StringTokenizer 추가

--- src/baekjoon/PROB1000.java
+++ src/baekjoon/PROB1000.java
@@ -9,7 +9,7 @@ public class PROB1000 {
     // 두 정수 A 와 B 를 입력받은 다음, A+B 를 출력하는 프로그램을 작성하세요.
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer str = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer str = new StringTokenizer(br.readLine());
 
         int A = Integer.parseInt(str.nextToken());
         int B = Integer.parseInt(str.nextToken());
