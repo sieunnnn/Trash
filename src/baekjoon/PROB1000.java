804bf2934a0e1f5437149e6de6e8429891d24f6a
시은 <119668620+sieunnnn@users.noreply.github.com>
2022-12-12 16:44:39 +0900

    [단계별] 입출력과 사칙연산

--- src/baekjoon/PROB1000.java
+++ src/baekjoon/PROB1000.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class PROB1000 {
+    
     // 두 정수 A 와 B 를 입력받은 다음, A+B 를 출력하는 프로그램을 작성하세요.
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
