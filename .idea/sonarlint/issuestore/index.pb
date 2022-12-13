9af21b2c78e87985f361b4084798a0563c9b652b
sieunnnn <wldsmtldsm65@gmail.com>
2022-12-13 12:49:19 +0900

    update gitignore

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+J
+src/baekjoon/PROB4673.java,3\3\33fbf6562f63a19d56c8af38ec3ea0b62e758257
+K
+src/baekjoon/PROB15596.java,1\e\1e7b92fdaac0a402e33064f6ec881116459665a8
+K
+src/baekjoon/PROB11654.java,3\3\33f7fb6f69d0cf77ac9c57d81e94306274643937
---
+++ src/multiCampus/objectOrientation/modifier/접근제한자/접근제한자test.java
@@ -0,0 +1,10 @@
+package multiCampus.objectOrientation.modifier.접근제한자;
+
+public class 접근제한자test {
+    public static void main(String[] args) {
+        접근제한자.publicMethod();
+        접근제한자.defaultMethod();
+        접근제한자.protectedMethod();
+
+    }
+}
