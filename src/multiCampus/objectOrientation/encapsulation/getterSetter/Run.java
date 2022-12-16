9e5aae99e153af33280174970b1cab8b407d295e
sieunnnn <wldsmtldsm65@gmail.com>
2022-12-16 14:31:50 +0900

    [멀티캠퍼스] getter/setter 수업내용

---
+++ src/multiCampus/objectOrientation/encapsulation/getterSetter/Run.java
@@ -0,0 +1,12 @@
+package src.multiCampus.objectOrientation.encapsulation.getterSetter;
+
+public class Run {
+
+    public static void main(String[] args) {
+        Coffee ame =  new Coffee();
+        ame.setName("아메리카노");
+        System.out.println(ame.getName());
+
+    }
+
+}
