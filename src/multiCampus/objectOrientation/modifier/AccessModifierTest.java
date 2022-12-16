eba8d4b77aadb1cec87a3ede22a67894401fd1f3
sieunnnn <wldsmtldsm65@gmail.com>
2022-12-16 14:17:35 +0900

    [멀티캠퍼스] 접근제한자 수업내용

---
+++ src/multiCampus/objectOrientation/modifier/AccessModifierTest.java
@@ -0,0 +1,10 @@
+package src.multiCampus.objectOrientation.modifier;
+
+public class AccessModifierTest {
+    public static void main(String[] args) {
+        AccessModifier.publicMethod();
+        AccessModifier.defaultMethod();
+        AccessModifier.protectedMethod();
+
+    }
+}
