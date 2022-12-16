fce9e403a6903efda164fddf9a7817ef732a80b4
sieunnnn <wldsmtldsm65@gmail.com>
2022-12-16 15:41:20 +0900

    [멀티캠퍼스] Initialization 수업내용

---
+++ src/multiCampus/objectOrientation/initialization/Run.java
@@ -0,0 +1,10 @@
+package src.multiCampus.objectOrientation.initialization;
+
+public class Run {
+
+    public static void main(String[] args) {
+        Initialization ini = new Initialization();
+        System.out.println(ini.getName());
+    }
+
+}
