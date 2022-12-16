d9b72828582656b5df87ceacdf7d9eb7d14e8ffe
sieunnnn <wldsmtldsm65@gmail.com>
2022-12-16 14:18:59 +0900

    [멀티캠퍼스] 메소드 수업내용

---
+++ src/multiCampus/objectOrientation/encapsulation/method/Run.java
@@ -0,0 +1,10 @@
+package src.multiCampus.objectOrientation.encapsulation.method;
+
+public class Run {
+    public static void main(String[] args) {
+
+        SpringConcert sc = new SpringConcert();
+        sc.startConcert();
+    }
+
+}
