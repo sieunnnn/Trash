35d3f42f9c49fecc03a172360dbd9aa40e1a55fb
sieunnnn <wldsmtldsm65@gmail.com>
2022-12-16 16:03:48 +0900

    [멀티캠퍼스] Class format 수업내용

---
+++ src/multiCampus/objectOrientation/classFormat/Run.java
@@ -0,0 +1,10 @@
+package src.multiCampus.objectOrientation.classFormat;
+
+public class Run {
+    public static void main(String[] args) {
+
+        Car car = new Car("BMW", "GOLD");
+
+    }
+
+}
