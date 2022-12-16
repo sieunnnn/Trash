5255633622907c44ceee7228c4ad96e74102f4d2
sieunnnn <wldsmtldsm65@gmail.com>
2022-12-16 16:33:11 +0900

    [멀티캠퍼스] overloading 수업내용

---
+++ src/multiCampus/objectOrientation/OverrideOverloading/Overloading/AreaCalculator.java
@@ -0,0 +1,19 @@
+package src.multiCampus.objectOrientation.OverrideOverloading.Overloading;
+
+public class AreaCalculator {
+    /*
+    Overload
+        (1) 과적하다, 넘치게 싣다.
+        (2) 하나의 이름에 메서드가 여러 개 존재
+        (3) Overload 는 매개변수의 개수나 타입이 다른 같은 이름의 메서드를 여러개 만드는 것을 의미한다.
+        (4) Overload 를 할 때는 두 메서드의 의미상의 기능이 동일 해야한다.
+    */
+    public static double calArea(Rectangle r) {
+        return r.getHeight() * r.getWidth();
+    }
+
+    public static double calArea(Circle c) {
+        return Math.PI * c.getRadius() * c.getRadius();
+    }
+
+}
