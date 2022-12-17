41b28dfd51a3efe9f7d19b997f1e9ddd02de02ec
sieunnnn <wldsmtldsm65@gmail.com>
2022-12-17 22:03:29 +0900

    [멀티캠퍼스] interface 수업내용

---
+++ src/multiCampus/objectOrientation/interface_/CommunicatableImpl.java
@@ -0,0 +1,13 @@
+package src.multiCampus.objectOrientation.interface_;
+
+public class CommunicatableImpl implements Communicatable {
+    @Override
+    public void sendMessage() {
+
+    }
+
+    @Override
+    public void receiveMessage() {
+
+    }
+}
