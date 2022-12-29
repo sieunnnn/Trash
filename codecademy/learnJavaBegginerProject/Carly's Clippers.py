ba43834b1101f4720a5902fa381f9769004cfc8d
sieunnnn <wldsmtldsm65@gmail.com>
2022-12-30 02:23:09 +0900

    반복문 연습 문제

---
+++ codecademy/learnJavaBegginerProject/Carly's Clippers.py
@@ -0,0 +1,37 @@
+#  calculating some important metrics that Carly can use to plan out the operation of the business for the rest of the month.
+
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+
+# Price and Cuts
+total_price = 0
+
+for price in prices :
+    total_price += price
+
+average_price = total_price / len(prices)
+
+print("Average Haircut Price: " + str(average_price))
+
+new_prices = [price - 5 for price in prices]
+print(new_prices)
+
+
+# Revenue
+total_revenue = 0
+
+for i in range(len(hairstyles)) :
+    total_revenue = prices[i] + last_week[i]
+
+print("Total Revenue: " + str(total_revenue))
+
+average_daily_revenue = total_revenue / 7
+
+cuts_under_30 = [new_prices[i] for i in range(len(new_prices)) if new_prices[i] < 30]
+print(cuts_under_30)
+
+
