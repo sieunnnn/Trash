981c363c32ac8f0a6c94b93ca15aabfc45e2fb94
sieunnnn <wldsmtldsm65@gmail.com>
2022-12-30 02:27:46 +0900

    Program which print out a continent and the largest city in that continent.

--- src/codecademy/learnJavaBegginerProject/ContinentsAndCities.java
+++ src/codecademy/learnJavaBegginerProject/ContinentsAndCities.java
@@ -1,6 +1,5 @@
 package codecademy.learnJavaBegginerProject;
 
-import static java.lang.System.out;
 
 public class ContinentsAndCities {
 
@@ -14,15 +13,32 @@ public class ContinentsAndCities {
         int continent = 4;
 
         switch (continent) {
-            case 1 -> out.println("North America: Mexico city, Mexico");
-            case 2 -> out.println("South America: Sao Paulo, Brazil");
-            case 3 -> out.println("Europe: Moscow, Russia");
-            case 4 -> out.println("Africa: Shanghai, china");
-            case 5 -> out.println("Asia: Shanghai, China");
-            case 6 -> out.println("Australia: Sydney, Australia");
-            case 7 -> out.println("Antarctica: McMurdoStation, US");
-            default -> out.println("Undefined continent!");
+            case 1:
+                System.out.println("North America: Mexico city, Mexico");
+                break;
+            case 2 :
+                System.out.println("South America: Sao Paulo, Brazil");
+                break;
+            case 3 :
+                System.out.println("Europe: Moscow, Russia");
+                break;
+            case 4 :
+                System.out.println("Africa: Shanghai, china");
+                break;
+            case 5 :
+                System.out.println("Asia: Shanghai, China");
+                break;
+            case 6 :
+                System.out.println("Australia: Sydney, Australia");
+                break;
+            case 7 :
+                System.out.println("Antarctica: McMurdoStation, US");
+                break;
+            default :
+                System.out.println("Undefined continent!");
+                break;
         }
+
     }
 
 }
