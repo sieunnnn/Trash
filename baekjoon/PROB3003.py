3f4ff7e01e5d484d93d02dd49f8341cc97c9cd5b
sieunnnn <wldsmtldsm65@gmail.com>
2023-01-05 11:19:24 +0900

    [단계별] 입출력과 사칙연산

--- baekjoon/PROB3003.py
+++ baekjoon/PROB3003.py
@@ -2,3 +2,8 @@
 # 동혁이가 발견한 흰색 피스의 개수가 주어졌을 때, 몇 개를 더하거나 빼야 올바른 세트가 되는지 구하는 프로그램을 작성하시오.
 
 # 배열과 반복문 배우고 풀기
+
+defualt = [1, 1, 2, 2, 2, 8]
+input_lst = list(map(int, input().split()))
+for i in range(6):
+    print(defualt[i] - input_lst[i], end=' ')
