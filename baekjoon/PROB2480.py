e9c7fc18d3c18900c7bd6c8fac6479ebda956d71
sieunnnn <wldsmtldsm65@gmail.com>
2023-01-13 09:55:45 +0900

    [단계별] 조건문

--- baekjoon/PROB2480.py
+++ baekjoon/PROB2480.py
@@ -4,15 +4,13 @@
 
 A, B, C = map(int, input().split())
 
-money = 0
-result = 0
-
-if (A == B and B == C) :
-    money = 10000 + A * 1000
-elif (A == B and B != C) :
-    money = 1000 + A * 100
-elif (A != B and B !=C) :
-    result = max(A, B, C)
-    money = max * 100
-
-print(money)
+if A == B == C:
+    print(10000+A*1000)
+elif A == B:
+    print(1000+A*100)
+elif A == C:
+    print(1000+A*100)
+elif B == C:
+    print(1000+B*100)
+else:
+    print(100 * max(A,B,C))
