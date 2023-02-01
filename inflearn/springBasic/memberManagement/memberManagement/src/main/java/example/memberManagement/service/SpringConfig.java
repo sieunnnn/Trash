a75bd89fa12ad244b0db45a5027dd509a7603405
sieunnnn <wldsmtldsm65@gmail.com>
2023-02-01 21:04:10 +0900

    인프런 강의 예제 따라하기

---
+++ inflearn/springBasic/memberManagement/memberManagement/src/main/java/example/memberManagement/service/SpringConfig.java
@@ -0,0 +1,35 @@
+package example.memberManagement.service;
+
+import example.memberManagement.repository.*;
+
+import example.memberManagement.service.MemberService;
+
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+
+
+@Configuration
+public class SpringConfig {
+
+    private final DataSource dataSource;
+
+
+    private final EntityManager em;
+
+    public SpringConfig(DataSource dataSource, EntityManager em) {
+        this.dataSource = dataSource;
+        this.em = em;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JpaMemberRepository(em);
+    }
+}
