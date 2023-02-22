9e0f3dc858083626a5c9e21e6e212031455b8b24
sieunnnn <wldsmtldsm65@gmail.com>
2023-02-22 10:55:39 +0900

    기본적인 구조완성 / Validation 진행 중

---
+++ ImageSalesSite/imageSalesSite/src/main/java/com/example/imageSalesSite/domain/CodeGroup.java
@@ -0,0 +1,37 @@
+package com.example.imageSalesSite.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@EqualsAndHashCode(of = "groupCode")
+@Table(name="code_group")
+public class CodeGroup {
+
+    // 유효성 검증 annotation 지정
+    @NotBlank
+    @Id
+    @Column(length = 3)
+    private String groupCode;
+
+    // 유효성 검증 annotation 지정
+    @NotBlank
+    @Column(length = 30, nullable = false)
+    private String groupName;
+
+    @Column(length = 1)
+    private String useYn = "Y";
+
+    @CreationTimestamp
+    private LocalDateTime regDate;
+    @UpdateTimestamp
+    private LocalDateTime updDate;
+}
