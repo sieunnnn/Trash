a7451ea62984ecdafad38c18f06f87d8626fbd94
sieunnnn <wldsmtldsm65@gmail.com>
2023-02-27 00:00:45 +0900

    페이징처리

--- ImageSalesSite/imageSalesSite/src/main/java/com/example/imageSalesSite/controller/BoardController.java
+++ ImageSalesSite/imageSalesSite/src/main/java/com/example/imageSalesSite/controller/BoardController.java
@@ -5,6 +5,7 @@ import com.example.imageSalesSite.domain.Member;
 import com.example.imageSalesSite.security.domain.CustomUser;
 import com.example.imageSalesSite.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -19,13 +21,13 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RequiredArgsConstructor
 @Controller
-@RequestMapping("/board")
+@RequestMapping(value = "/board")
 public class BoardController {
 
     private final BoardService service;
 
     // 등록 화면
-    @GetMapping("/register")
+    @GetMapping(value = "/register")
     @PreAuthorize("hasRole('MEMBER')")
     public void registerForm(Model model, Authentication authentication) throws Exception {
 
@@ -41,7 +43,7 @@ public class BoardController {
     }
 
     // 등록 처리
-    @PostMapping("/register")
+    @PostMapping(value = "/register")
     @PreAuthorize("hasRole('MEMBER')")
     public String register(@Validated Board board, BindingResult result, RedirectAttributes rttr) throws Exception {
 
@@ -56,50 +58,95 @@ public class BoardController {
         return "redirect:/board/list";
     }
 
-    // 목록 화면
-    @GetMapping("/list")
-    public void list(Model model) throws Exception {
-        model.addAttribute("list", service.list());
+//    // 목록 화면
+//    @GetMapping(value = "/list")
+//    public void list(Model model) throws Exception {
+//        model.addAttribute("list", service.list());
+//    }
+
+    // 페이징 요청 정보를 매개변수로 받고 다시 뷰에 전달한다.
+    @GetMapping(value = "/list")
+    public void list(@ModelAttribute("pgrq") PageRequestVO pageRequestVO, Model model) throws Exception {
+        Page<Board> page = service.list(pageRequestVO);
+
+        model.addAllAttributes("pgntn" new PaginationDTO<Board>(page));
     }
 
-    // 상세 화면
-    @GetMapping("/read")
-    public void read(Long boardNo, Model model) throws Exception {
+//    // 상세 화면
+//    @GetMapping(value = "/read")
+//    public void read(Long boardNo, Model model) throws Exception {
+//        model.addAttribute(service.read(boardNo));
+//    }
+
+    // 페이징 요청 정보를 매개변수로 받고 다시 뷰에 전달한다.
+    @GetMapping(value = "/read")
+    public void read(Long boardNode, @ModelAttribute("parq") PageRequestVO pageRequestVO, Model model) throws Exception {
         model.addAttribute(service.read(boardNo));
     }
 
-    // 삭제 처리
-    @PostMapping("/remove")
-    @PreAuthorize("(hasRole('MEMBER') and principal.username == #writer) or hasRole('ADMIN')")
-    public String remove(Long boardNo, RedirectAttributes rttr, String writer) throws Exception {
+//    // 삭제 처리
+//    @PostMapping(value = "/remove")
+//    @PreAuthorize("(hasRole('MEMBER') and principal.username == #writer) or hasRole('ADMIN')")
+//    public String remove(Long boardNo, RedirectAttributes rttr, String writer) throws Exception {
+//
+//        service.remove(boardNo);
+//
+//        rttr.addFlashAttribute("msg", "SUCCESS");
+//
+//        return "redirect:/board/list";
+//    }
+
+    // 페이징 요청 정보를 매개변수로 받고 다시 뷰에 전달한다.
+    @PostMapping(value = "/remove")
+    @PreAuthorize("(hasRole('MEMBER') and principal.username == #writer)or hasRole('ADMIN')")
+    public String remove(Long boardNo, PageRequestVO pageRequestVO, RedirectAttributes rttr, String writer) throws Exception {
 
         service.remove(boardNo);
 
+        rttr.addFlashAttribute("page", pageRequestVO.getPage());
+        rttr.addFlashAttribute("sizePerPage",pageRequestVO.getSizePerPage());
+
         rttr.addFlashAttribute("msg", "SUCCESS");
 
         return "redirect:/board/list";
     }
 
-    // 수정 화면
-    @GetMapping("/modify")
+//    // 수정 화면
+//    @GetMapping("/modify")
+//    @PreAuthorize("hasRole('MEMBER')")
+//    public void modifyForm(Long boardNo, Model model) throws Exception {
+//        model.addAttribute(service.read(boardNo));
+//    }
+
+    // 페이징 요청 정보를 매개변수로 받고 다시 뷰에 전달한다.
+    @GetMapping(value = "/modify")
     @PreAuthorize("hasRole('MEMBER')")
-    public void modifyForm(Long boardNo, Model model) throws Exception {
+    public void modifyForm(Long boardNo, @ModelAttribute("pgrq")PageRequestVO pageRequestVO, Model model) throws Exception {
         model.addAttribute(service.read(boardNo));
     }
 
-    // 수정 처리
-    @PostMapping("/modify")
+//    // 수정 처리
+//    @PostMapping("/modify")
+//    @PreAuthorize("hasRole('MEMBER') and principal.username == #board.writer")
+//    public String modify(@Validated Board board, BindingResult result, RedirectAttributes rttr) throws Exception {
+//
+//        if(result.hasErrors()) {
+//            return "board/modify";
+//        }
+//
+//        service.modify(board);
+//
+//        rttr.addFlashAttribute("msg", "SUCCESS");
+//
+//        return "redirect:/board/list";
+//    }
+
     @PreAuthorize("hasRole('MEMBER') and principal.username == #board.writer")
-    public String modify(@Validated Board board, BindingResult result, RedirectAttributes rttr) throws Exception {
+    public String modify(@Validated Board board, BindingResult result, @ModelAttribute("pgrq")
+    PageRequestVO pageRequestVO, RedirectAttributes rttr) throws Exception {
 
-        if(result.hasErrors()) {
-            return "board/modify";
+        if (result.hasErrors()) {
+
         }
-
-        service.modify(board);
-
-        rttr.addFlashAttribute("msg", "SUCCESS");
-
-        return "redirect:/board/list";
     }
 }
