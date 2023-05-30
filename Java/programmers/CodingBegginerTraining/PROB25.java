0c91f7c79e0d43ff0393b357f59b9cecf9f66e50
sieunnnn <wldsmtldsm65@gmail.com>
2023-05-30 23:01:48 +0900

    코딩테스트 입문

---
+++ Java/programmers/CodingBegginerTraining/PROB25.java
@@ -0,0 +1,17 @@
+package programmers.CodingBegginerTraining;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class PROB25 {
+
+    public static void main(String[] args) {
+        String s = "abcabcadc";
+
+        System.out.println(solution(s));
+    }
+
+    public static String solution(String s) {
+
+    }
+}
